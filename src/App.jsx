8e19b4362321b6de81b5c46f3795b099f4d60ebe
Rafael138 <85367193+Rafael138@users.noreply.github.com>
2023-09-21 01:28:36 +0000

    Alteracoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+    <div className="card">
+        <h2>Ana</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Fabricio</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <h2>Rafael</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/5.jpeg" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <h2>fabio</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/7.jpeg" />
+      </div>
+      <div>
+        <h2>Paulo Andre</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/8.jpeg" />
+      </div>
     </>
   )
 }
